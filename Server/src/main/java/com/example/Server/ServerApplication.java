d7df0ff674ff4003f1c3ae935844d6d09bbb90e8
revoly afanasev <rewoly@mail.ru>
2024-12-23 21:56:36 +0900

    Начало

--- Server/src/main/java/com/example/Server/ServerApplication.java
+++ Server/src/main/java/com/example/Server/ServerApplication.java
@@ -2,12 +2,12 @@ package com.example.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Точка запуска приложения
+ */
 @SpringBootApplication
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
