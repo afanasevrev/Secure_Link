19fbdb0b0e9be81b7c82d267b55899b7adeeacd5
revoly afanasev <rewoly@mail.ru>
2024-12-24 22:11:06 +0900

    Работа с формой отправки сообщения

--- Server/src/main/java/com/example/Server/Controllers/WebController.java
+++ Server/src/main/java/com/example/Server/Controllers/WebController.java
@@ -2,7 +2,6 @@ package com.example.Server.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 /**
  * Обработка запросов от клиентов
  */
