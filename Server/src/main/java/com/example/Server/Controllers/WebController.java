9229b64d3355507041b44e5d6a7c83a23b2ea67b
revoly afanasev <rewoly@mail.ru>
2024-12-27 22:56:03 +0900

    Работа с формой отправки сообщений

--- Server/src/main/java/com/example/Server/Controllers/WebController.java
+++ Server/src/main/java/com/example/Server/Controllers/WebController.java
@@ -1,7 +1,10 @@
 package com.example.Server.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 /**
  * Обработка запросов от клиентов
  */
@@ -10,5 +13,10 @@ public class WebController {
     @GetMapping("/sendMessage")
     private String sendMessage() {
         return "send_message";
+    }
+    @PostMapping("/sendMessage")
+    private String sendMessage(Model model) {
+        System.out.println("Получено сообщение");
+        return "Получено Post - сообщение";
     }
 }
