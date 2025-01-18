7e8113c1aa2027cba025d7cd1580ead59c9424c5
revoly afanasev <rewoly@mail.ru>
2025-01-19 00:18:16 +0900

    Recipient достала

--- Server/src/main/java/com/example/Server/Controllers/WebController.java
+++ Server/src/main/java/com/example/Server/Controllers/WebController.java
@@ -4,6 +4,7 @@ import com.example.Server.MessageForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 /**
  * Обработка запросов от клиентов
@@ -16,8 +17,9 @@ public class WebController {
         return "send_message";
     }
     @PostMapping("/sendMessage")
-    private String sendMessage() {
-        System.out.println("Получено сообщение");
-        return "Получено Post - сообщение";
+    private String sendMessage(@ModelAttribute MessageForm messageForm, Model model) {
+        model.addAttribute("sendMessage", messageForm);
+        System.out.println("Posted message " + messageForm.getMessage());
+        return "send_message";
     }
 }
