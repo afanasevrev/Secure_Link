51269228fa4b78b74263dc4e45678c911211ed96
revoly afanasev <rewoly@mail.ru>
2025-01-19 00:02:18 +0900

    Recipient достала

--- Server/src/main/java/com/example/Server/MessageForm.java
+++ Server/src/main/java/com/example/Server/MessageForm.java
@@ -1,11 +1,19 @@
 package com.example.Server;
 
-import lombok.Getter;
-import lombok.Setter;
-@Getter
-@Setter
 public class MessageForm {
     private String recipient;
     private String message;
     public MessageForm() {}
+    public String getRecipient() {
+        return recipient;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
