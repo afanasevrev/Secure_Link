83a0b924f7a7f82624a7788c5f777c7bd9fc67ae
revoly afanasev <rewoly@mail.ru>
2025-01-19 22:53:13 +0900

    Работа с криптографией

--- Server/src/main/java/com/example/Server/Controllers/WebController.java
+++ Server/src/main/java/com/example/Server/Controllers/WebController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 /**
  * Обработка запросов от клиентов
  */
